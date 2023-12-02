8bb74777af491b32aecdd6927d397d325aae4235
celinaetienot <celinaetienot@gmail.com>
2023-12-02 14:53:58 -0300

    listar actualizado

--- listado.js
+++ listado.js
@@ -1,39 +1,38 @@
-const URL = "http://127.0.0.1:5000/"
+const URL = "http://127.0.0.1:5000/";
 
 // Realizamos la solicitud GET al servidor para obtener todos los documentos
-
 fetch(URL + 'documentos')
     .then(function (response) {
         if (response.ok) {
             return response.json();
-    } else {
-         // Si hubo un error, lanzar explícitamente una excepción
-        // para ser "catcheada" más adelante
-            throw new Error('Error al obtener los productos.');
+        } else {
+            // Si hubo un error, lanzar explícitamente una excepción
+            // para ser "catcheada" más adelante
+            throw new Error('Error al obtener los documentos.');
         }
     })
     .then(function (data) {
-        let tablaProductos = document.getElementById('tablaProductos');
+        let tableBody = document.getElementById('tableBody');
 
-    // Iteramos sobre los productos y agregamos filas a la tabla
+        // Iterate over the documents and create rows
         for (let documento of data) {
-            let fila = document.createElement('tr');
-            fila.innerHTML = '<td>' + documento.No + '</td>' +
-                            '<td>' + documento.Title + '</td>' +
-                            '<td align="right">' + producto.cantidad + '</td>' +
-                            '<td align="right">' + producto.precio + '</td>' +
-    // Mostrar miniatura de la imagen
-    '<td><img src=static/img/' + producto.imagen_url +' 
-    alt="Imagen del producto" style="width: 100px;"></td>' +
-    '<td align="right">' + producto.proveedor + '</td>';
-    //Una vez que se crea la fila con el contenido del producto, 
-    se agrega a la tabla utilizando el método appendChild del elemento
-    tablaProductos.
-    tablaProductos.appendChild(fila);
-    }
+            // Create a table row
+            let row = document.createElement('tr');
+
+            // Add columns to the row
+            row.innerHTML = `<td>${documento.No}</td>
+                             <td>${documento.Title}</td>
+                             <td>${documento.Area}</td>
+                             <td>${documento.Last_valid_version}</td>
+                             <td>${documento.Igall_owner}</td>
+                             <td><a href="static/documentos/${documento.url}" download>Descargar</a></td>`;
+
+            // Append the row to the table body
+            tableBody.appendChild(row);
+        }
     })
     .catch(function (error) {
-    // En caso de error
-    alert('Error al agregar el producto.');
-    console.error('Error:', error);
-    }
+        // En caso de error
+        alert('Error al obtener los documentos.');
+        console.error('Error:', error);
+    });
